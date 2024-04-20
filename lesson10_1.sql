f81a45bddd7715d70629e0f8eb7cd13b69ec4a6e
賴韻安~ <a411153@gmail.com~>
2024-04-20 14:13:28 +0800

    modify

--- lesson10_1.sql
+++ lesson10_1.sql
@@ -1,4 +1,4 @@
-DROP TABLE student;
+DROP TABLE IF EXISTS student;
 
 CREATE TABLE IF NOT EXISTS student(
 	id SERIAL PRIMARY KEY,
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS student(
 	english SMALLINT,
 	math SMALLINT
 );
+
+INSERT INTO student(name, chinese, english, math)
+VALUES('賴安安',70,80,90);
+
+SELECT *
+FROM student
